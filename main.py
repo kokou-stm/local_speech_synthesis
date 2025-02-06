1e665001e7be296c9676e52196e6c41b3d54d9b4
kokou-stm <kokou.sekpona@uphf.fr>
2025-02-06 12:35:41 +0100

    update requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
+
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from transformers import VitsModel, AutoTokenizer
@@ -6,6 +6,7 @@ import torch
 import scipy.io.wavfile
 from pydantic import BaseModel
 from io import BytesIO
+import numpy as np
 
 # Charger le modèle et le tokenizer
 checkpoint = "facebook/mms-tts-ewe"
@@ -18,7 +19,7 @@ app = FastAPI()
 
 @app.get('/')
 def first():
-    return {"Hello": "man"}
+    return {"Hello": "tts"}
 
 #Predict:
 
@@ -29,14 +30,13 @@ def predict(message:str):
     with torch.no_grad():
         output = model(**input).waveform
     audio_np = output.squeeze(0).cpu().numpy()
-    #print("Adio: ", audio_np, type(audio_np))
-    # Enregistrer le fichier audio en mémoire
+    
     wav_io = BytesIO()
-    #scipy.io.wavfile.write(wav_io, 22050, audio_np.astype(np.float32))
+    
     scipy.io.wavfile.write(wav_io, rate=model.config.sampling_rate, data=(audio_np * 32767).astype(np.int16))
     wav_io.seek(0)  # Rewind pour la lecture
 
-    # Retourner les données audio sous forme de flux pour que le client puisse lire le fichier
+    
     return StreamingResponse(wav_io, media_type="audio/wav")
-    #return audio_np
+
 
